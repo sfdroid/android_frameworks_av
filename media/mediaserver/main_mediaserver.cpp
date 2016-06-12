821e4db0d032ed7c251830c7c9428f447461c884
Frajo Haider <f_haider@gmx.at>
2016-06-12 11:42:10 +0200

    [sfdroid]: disable MediaPlayerService and CameraService in mediaserver since sfos minimediaservice has those
    Change-Id: Id28a222ec5fd16c6b55dcb5ccbae626e39572245

--- media/mediaserver/main_mediaserver.cpp
+++ media/mediaserver/main_mediaserver.cpp
@@ -129,8 +129,8 @@ int main(int argc __unused, char** argv)
         sp<IServiceManager> sm = defaultServiceManager();
         ALOGI("ServiceManager: %p", sm.get());
         AudioFlinger::instantiate();
-        MediaPlayerService::instantiate();
-        CameraService::instantiate();
+        //MediaPlayerService::instantiate();
+        //CameraService::instantiate();
 #ifdef AUDIO_LISTEN_ENABLED
         ALOGI("ListenService instantiated");
         ListenService::instantiate();
